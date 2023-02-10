674576091562cdf8ba78e7a4ab82a9785b8c096e
Jongtack Kim <jongtack.kim@gmail.com>
2023-02-10 11:37:57 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

--- main.py
+++ main.py
@@ -1,51 +1,15 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget,QPushButton,
-                        QVBoxLayout,QHBoxLayout,
-                        QMessageBox,QPlainTextEdit)
-from PyQt5.QtGui import QIcon
+from ui import View # ui.py의 View 클래스 추가
+from ctrl import Control # ctrl.py의 Control 클래스 추가
+from PyQt5.QtWidgets import QApplication
 
-class Calculator(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
-
-        self.btn1 = QPushButton('Message', self)
-        self.btn1.clicked.connect(self.activateMessage)
-        self.btn2 = QPushButton('Clear', self)
-        self.btn2.clicked.connect(self.clearMessage)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.te1)
-        #vbox.addWidget(self.btn1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox)
-
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256, 256)
-        self.show()
-
-    def activateMessage(self):
-        #QMessageBox.information(self, 'information', 'Button clicked!')
-        self.te1.appendPlainText('Button clicked!')
-
-    def clearMessage(self):
-        self.te1.clear()
+def main():
+    calc = QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    view = View() # View 인스턴스 선언
+    Control(view=view)
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
-    sys.exit(app.exec_())
+    main()
