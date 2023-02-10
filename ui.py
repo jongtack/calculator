aa81b5430c95ecbf960d94f98d90608bb62c9d71
Jongtack Kim <jongtack.kim@gmail.com>
2023-02-10 12:52:05 +0900

    Add lbl1 in ui.py to print current date

--- ui.py
+++ ui.py
@@ -1,15 +1,19 @@
 from PyQt5.QtWidgets import (QApplication, QWidget,QPushButton,
                         QVBoxLayout,QHBoxLayout,
-                        QMessageBox,QPlainTextEdit)
+                        QMessageBox,QPlainTextEdit,
+                        QLabel)
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QDate, Qt
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.date = QDate.currentDate()
         self.initUI()
 
     def initUI(self):
+        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
@@ -24,6 +28,7 @@ class View(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
+        vbox.addWidget(self.lbl1)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
