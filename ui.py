45572c865323fa5c5df73f4360bac9f7833d34af
developer <dev@jtsoft.co.kr>
2023-02-13 20:09:00 +0900

    Modify btn1, le1 and method name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -17,6 +17,8 @@ class View(QWidget):
 
         self.le1 = QLineEdit('0', self)
         self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le1.setFocus(True)
+        self.le1.selectAll()
 
         self.le2 = QLineEdit('0', self)
         self.le2.setAlignment(QtCore.Qt.AlignRight)
@@ -29,7 +31,7 @@ class View(QWidget):
         hbox_formular.addWidget(self.cb)
         hbox_formular.addWidget(self.le2)
 
-        self.btn1 = QPushButton('Message', self)
+        self.btn1 = QPushButton('Calc', self)
         self.btn2 = QPushButton('Clear', self)
 
         hbox = QHBoxLayout()
@@ -50,7 +52,7 @@ class View(QWidget):
         self.resize(256, 256)
         self.show()
 
-    def activateMessage(self):
+    def setDisplay(self):
         self.te1.appendPlainText('Button clicked!')
 
     def clearMessage(self):
